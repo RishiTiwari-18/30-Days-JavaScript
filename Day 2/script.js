f6b4e50c171d6cc68fd23287e9036f579922c8c6
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-15 23:30:07 +0530

    .

--- Day 2/script.js
+++ Day 2/script.js
@@ -47,7 +47,28 @@
 // console.log(number);
 
 
-//? Task 8
-let num1 = 4;
-let num2 = 5;
-console.log(num1 >  num2);
+// //? Task 8
+
+// let num1 = 4;
+// let num2 = 5;
+// console.log(num1 >  num2)
+// console.log(num1 < num2)
+
+
+// //? Task 9
+
+// let num1 = 44;
+// let num2 = 55;
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+
+// //? Task 10
+
+// let num1 = 22
+// let num2 = 22
+// console.log(num1 === num2);
+// //The == operator in JavaScript performs loose equality. It converts the variable values to the same type before comparison (a process known as type coercion). On the other hand, the === operator (also called strict equality or triple equals) compares both the values and the data types of the operands directly. It does not perform any type conversion
+
+
+//? Task 11
