11c6617d70f652c06dd22b53ea043966ff61853a
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-15 20:16:07 +0530

    1 complete

--- Day 2/script.js
+++ Day 2/script.js
@@ -31,3 +31,17 @@
 // const num1 = 44;
 // const num2 = 4;
 // console.log(num1 % num2);
+
+
+// //? Task 6
+
+// let number = 11;
+// number += 22;
+// console.log(number);
+
+
+// //? Task 7 
+
+// let number = 11;
+// number -= 22;
+// console.log(number);
