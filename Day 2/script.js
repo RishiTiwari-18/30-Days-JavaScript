8c0c248b588748b0923b334c29e1857ebfe8ef77
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-15 20:06:08 +0530

    Activity 1 complete

---
+++ Day 2/script.js
@@ -0,0 +1,33 @@
+// //? Task 1
+
+// const num1 = 5;
+// const num2 = 15;
+// console.log(`The sum of numbers is ${num1 + num2}`);
+
+
+// //? Task 2
+
+// const num1 = 3;
+// const num2 = 9;
+// console.log(num1 - num2);
+
+
+// //? Task 3
+
+// const num1 = 2;
+// const num2 = 4;
+// console.log(num1 * num2);
+
+
+// //? Task 4
+
+// const num1 = 44;
+// const num2 = 4;
+// console.log(num1 / num2);
+
+
+// //? Task 5
+
+// const num1 = 44;
+// const num2 = 4;
+// console.log(num1 % num2);
