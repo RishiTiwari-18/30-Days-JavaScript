a48eeff7741dca8bd59a6fc840ef2c9f116cf2c1
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-17 00:04:05 +0530

    Task1

---
+++ Day 3/script.js
@@ -0,0 +1,16 @@
+// //? Task 1
+
+// function check(num){
+//     if(num > 0){
+//         console.log("Number is positive");
+//     }else if(num === 0 ){
+//         console.log("Number is Zero");
+//     }else if(num < 0){
+//         console.log("Number is negative");
+//     }else{
+//         console.log("Given number is NaN");
+//     }
+
+// }
+
+// check(6)
