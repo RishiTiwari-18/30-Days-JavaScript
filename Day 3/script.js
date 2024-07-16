448b97cd270b01adf4c854912b4df5510cbd5e2a
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-17 00:24:41 +0530

    .

--- Day 3/script.js
+++ Day 3/script.js
@@ -44,40 +44,40 @@
 // numbers(13, 16, 8)
 
 
-//? Task 4
+// //? Task 4
 
-const day = (e) => {
-    switch (e) {
-        case 1:
-            console.log("Monday");
-            break;
+// const day = (e) => {
+//     switch (e) {
+//         case 1:
+//             console.log("Monday");
+//             break;
 
-        case 2:
-            console.log("Tuesday");
-            break;
-        case 3:
-            console.log("Wednesday");
-            break;
+//         case 2:
+//             console.log("Tuesday");
+//             break;
+//         case 3:
+//             console.log("Wednesday");
+//             break;
 
-        case 4:
-            console.log("Thursday");
-            break;
+//         case 4:
+//             console.log("Thursday");
+//             break;
 
-        case 5:
-            console.log("Friday");
-            break;
+//         case 5:
+//             console.log("Friday");
+//             break;
 
-        case 6:
-            console.log("Saturday");
-            break;
+//         case 6:
+//             console.log("Saturday");
+//             break;
 
-        case 7:
-            console.log("Sunday");
-            break;
-        default:
-            console.log("You serious!");
-            break;
-    }
-}
+//         case 7:
+//             console.log("Sunday");
+//             break;
+//         default:
+//             console.log("You serious!");
+//             break;
+//     }
+// }
 
-day(7)
+// day(7)
