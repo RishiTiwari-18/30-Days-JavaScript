ed12d1070a86b27c81d61ed1e6d705bf8baa6736
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-17 16:41:51 +0530

    Task 123

---
+++ Day 4/script.js
@@ -0,0 +1,25 @@
+// //? Task 1
+
+// for( let i = 1; i <= 10; i++ ){
+//     console.log(i);
+// }
+
+
+// //? Task 2
+
+//     for( i = 1; i <= 10; i++){
+//         console.log(i*5);
+//     }
+
+
+// //? Task 3
+
+// let num = 1;
+// let total = 0;
+// while(num <= 10){
+//     total += num ;
+//     console.log(total);
+//     num++
+// }
+
+
