81659998587ec00d00b4d9c49b626aca752a7c9a
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-19 00:05:48 +0530

    Day 5 task 1

---
+++ Day 5/script.js
@@ -0,0 +1,11 @@
+// //? Task 1
+
+// function calculation(num){
+//     if(num % 2 === 0){
+//         console.log("Number is even");
+//     }else{
+//         console.log("Number is odd");
+//     }
+// }
+
+// calculation(3)
