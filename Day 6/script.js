6b8cdf4d77da76a71706931aff9cece6b5d7c0bd
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-19 23:54:10 +0530

    complete

---
+++ Day 6/script.js
@@ -0,0 +1,106 @@
+// //? Task 1
+
+// let arr = [1, 2, 3, 4 ,5]
+// console.log(arr);
+
+
+// //? Task 2
+
+// let arr = [1, 2, 3, 4, 5]
+// let first = arr[0]
+// let last = arr[arr.length-1]
+// console.log(first,  last);
+
+
+// //? Task 3
+
+// let arr1 = [1, 2, 3, 4 ,5]
+// arr1.push(5)
+// console.log(arr1);
+
+
+// //? Task 4
+
+// let arr2 = [1, 2, 3, 4 ,5]
+// arr2.pop(5)
+// console.log(arr2);
+
+
+// //? Task 5
+
+// let arr3 = [1, 2, 3, 4 ,5]
+// arr3.shift(5)
+// console.log(arr3);
+
+
+// //? Task 6
+
+// let arr4 = [1, 2, 3, 4 ,5]
+// arr4.unshift(5)
+// console.log(arr4);
+
+
+// //? Task 7
+
+// let arr = [1, 2, 3, 4 ,5]
+// let newArr = arr.map((num) => num * 2)
+// console.log(newArr);
+
+
+// //? Task 8
+
+// let arr = [1, 2, 3, 4 ,5, 6, 7, 8, 44]
+// let newArr = arr.filter((item) =>  item % 2 === 0)
+// console.log(newArr);
+
+
+// //? Task 9
+
+// const arr = [1, 2, 3, 4]
+// const sum = arr.reduce((acc, curr,) => acc + curr + 0) ;
+// console.log(sum);
+
+
+// //? Task 10
+
+// let arr = [1, 2, 3, 4]
+// for(i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// // let arr = [1, 2, 3, 4]
+// // arr.map((item) => console.log(item))    //Using map
+
+
+// //? Task 11
+
+// let arr = [1, 2, 3, 4, 5]
+// arr.forEach((item) => {
+//     console.log(item);
+// })
+
+
+// //? Task 12
+
+// let arr = [
+//     ["Name1", 0],
+//     ["Name2", 0],
+//     ["Name3", 0],
+//     ["Name4", 0],
+//     ["Name1", 0],
+// ]
+// console.log(arr);
+
+
+// //? Task 13
+
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// console.log(matrix[1][1]);
+
+
+
+
