582546cc6876592e31741588ec2b103276898293
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-20 00:10:16 +0530

    .

---
+++ Day 7/Script.js
@@ -0,0 +1,9 @@
+// //? Task 1, 2
+// let books = {
+//     Title: "What we cannot know",
+//     Autor: "Marcus du Soutoy",
+//     year: 2024
+// }
+
+// console.log(books.Title,);
+// console.log(books.Autor);
