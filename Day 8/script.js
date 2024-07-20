4942e631e7d64960beac03aeadceda03919fabe5
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-20 16:54:20 +0530

    .

---
+++ Day 8/script.js
@@ -0,0 +1,40 @@
+// //? Task 1
+
+// let name = "Loki"
+// let age = "19"
+// console.log(`hey ${name} your age is ${age}`);
+
+
+// //? Task 2
+
+// let string = `loki
+// 20
+// 2005`
+// console.log(string);
+
+
+// //? Task 3
+
+// let elem = ["a" , "b", "c", "d"]
+
+// let [elem1, elem2] = elem
+// console.log(elem1);
+// console.log(elem2);
+
+
+// //? Task 4
+
+// let books = {
+//     Title: "What we cannot know",
+//     Autor: "Marcus du Soutoy",
+//     year: 2024
+// }
+
+// let {Title, Autor} = books
+// console.log(Title);
+// console.log(Autor);
+
+
+//? Task 5
+
+
