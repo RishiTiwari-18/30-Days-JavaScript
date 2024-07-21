34520ae96038846b9a73b55970b8679d15b4850d
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-22 00:07:05 +0530

    .

--- Day 7/Script.js
+++ Day 7/Script.js
@@ -9,4 +9,39 @@
 // console.log(books.Autor);
 
 
-//? Task 3
+// //? Task 3, 4
+
+// class books  {
+//     constructor(Title, Author, year ){
+//         this.Title = Title
+//         this.Author = Author
+//         this.year = year
+//     }
+
+//     info(){
+//         return {Title, Author, year}
+//     }
+
+// }
+
+// const user = new books("What we cannot know", "Marcus du Soutoy", 2020)
+
+// console.log(user.Author);
+// console.log(user.Title);
+// console.log(user.year);
+
+
+//? Task 5
+
+const library = {
+    name: " Central Library",
+    location: "Delhi",
+    book:[
+        {}
+    ]
+
+}
+
+
+//? Task 4
+
