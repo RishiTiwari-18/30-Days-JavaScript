f9225ea4b840fcd7e09431904adf9a5ba6be4655
RishiTiwari-18 <rishitiwari372@gmail.com>
2024-07-24 10:40:44 +0530

    task 4

--- Day 9/script.js
+++ Day 9/script.js
@@ -1,3 +1,25 @@
+//? Task 1
+
 let greet = document.querySelector(".greet")
 greet.textContent = "hieee"
 
+
+//? Task 2
+
+let greet2 = document.querySelector("#greet")
+greet2.textContent = "hiii"
+
+
+//? Task 3
+
+let newDiv  = document.createElement("div")
+newDiv.textContent = "new div"
+document.body.appendChild(newDiv)
+
+
+//? Task 4
+
+let ul = document.querySelector("ul")
+let newLi = document.createElement("li")
+newLi.textContent = "new li"
+ul.appendChild(newLi)
